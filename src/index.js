d7c67a79d45faf1eb4cd4ffe74101dc701b44093
Sujoy Ghosh <60333881+SujoyGhosh-scott@users.noreply.github.com>
2024-05-19 13:13:28 +0530

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { ButtonToolbar, DropdownButton } from "react-bootstrap";
+console.log("hello")
 
 class Box extends React.Component {
   selectBox = () => {
